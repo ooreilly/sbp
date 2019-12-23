7e5c9a56a7155b0f510e489bdaf28cf8ae85c38e
Ossian O'Reilly <ossian.oreilly@gmail.com>
2019-12-22 19:12:02 -0800

    Add script that zips results.

--- covariant/plot/efficiency_plot.py
+++ covariant/plot/efficiency_plot.py
@@ -8,65 +8,40 @@ grid0=0
 gridn=6
 ngrid = gridn - grid0 + 1
 ctask="timing/collocated/output_default"
-sdtask="timing/staggered/output_default"
 smtask="timing/staggered/output_modified"
 
 ctime="timing_stencil/collocated/output_modified"
 smtime="timing_stencil/staggered/output_modified"
 
-# collocated, staggered modified, staggered default data
+# collocated, staggered
 c = []
 sm = []
-sd = []
 
 # Configs for efficiency plot
 ecc = []
 ecm = []
 
-# Configs for convergence plot
-cc = []
-cm = []
-cd = []
-
 for num in range(grid0, gridn):
     c.append(helper.load_csv("%s_%d" % (ctask, num), "u.csv"))
     sm.append(helper.load_csv("%s_%d" % (smtask, num), "u.csv"))
 
-    sd.append(helper.load_csv("%s_%d" % (sdtask, num), "u.csv"))
     ecc.append(helper.load_config("%s_%d" % (ctime, num), "log.txt"))
     ecm.append(helper.load_config("%s_%d" % (smtime, num), "log.txt"))
-
-    cc.append(helper.load_config("%s_%d" % (ctask, num), "log.txt"))
-    cm.append(helper.load_config("%s_%d" % (smtask, num), "log.txt"))
-    cd.append(helper.load_config("%s_%d" % (sdtask, num), "log.txt"))
 
 for num in range(gridn, gridn+1):
     ref = helper.load_csv("%s_%d" % (smtask, num), "u.csv")
     c.append(ref)
     sm.append(ref)
-    sd.append(ref)
 
 pc = helper.compute_errors(c, 1)
 pm = helper.compute_errors(sm, 1)
-pd = helper.compute_errors(sd, 1)
 
-xc = []
-xm = []
-xd = []
 tc = []
 tm = []
-td = []
-err_c = []
-err_m = []
-err_d = []
 
 for num in range(grid0, gridn):
     tc.append(ecc[num].avg)
     tm.append(ecm[num].avg)
-
-    xc.append(cc[num].m)
-    xm.append(cm[num].m)
-    xd.append(cd[num].m)
 
 fig, ax = plt.subplots()
 ax.loglog(tc, pc.norm_rel_err, 'o-', label="collocated")
